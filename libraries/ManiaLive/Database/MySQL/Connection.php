437db8291c838e810237a3084f9e18eac3adb47a
Philippe Melot <melot.philippe@gmail.com>
2010-12-22 15:31:50 +0000

    * add query format for database queries
    git-svn-id: http://manialive.googlecode.com/svn/trunk@37 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLive/Database/MySQL/Connection.php
+++ libraries/ManiaLive/Database/MySQL/Connection.php
@@ -17,7 +17,7 @@ class Connection extends \ManiaLive\Database\Connection
 	protected $user;
 	protected $password;
 	protected $database;
-	
+
 	function __construct($host, $username, $password, $database, $port)
 	{
 		// Init
@@ -26,33 +26,33 @@ class Connection extends \ManiaLive\Database\Connection
 		$this->password = $password;
 		$this->clientFlags = 0;
 		$this->referenceCount = 0;
-		
+
 		// Connection
 		try
 		{
 			$this->connection = mysql_connect(
-				$this->host,
-				$this->user,
-				$this->password
+			$this->host,
+			$this->user,
+			$this->password
 			);
 		}
 		catch(\ErrorException $err)
 		{
 			throw new ConnectionException($err->getMessage(), $err->getCode());
 		}
-				
+
 		// Success ?
 		if(!$this->connection)
 		{
 			throw new ConnectionException;
 		}
-		
-		$this->select($database);	
-		
+
+		$this->select($database);
+
 		// Default Charset : UTF8
 		self::setCharset('utf8');
 	}
-	
+
 	function setCharset($charset)
 	{
 		if(function_exists('mysql_set_charset'))
@@ -68,7 +68,7 @@ class Connection extends \ManiaLive\Database\Connection
 			$this->execute('SET NAMES '.$charset);
 		}
 	}
-	
+
 	function select($database)
 	{
 		if($database != $this->database)
@@ -80,12 +80,12 @@ class Connection extends \ManiaLive\Database\Connection
 			}
 		}
 	}
-		
+
 	function quote($string)
 	{
 		return '\''.mysql_real_escape_string($string, $this->connection).'\'';
 	}
-	
+
 	/**
 	 * @param string The query
 	 * @return DatabaseRecordSet
@@ -93,43 +93,51 @@ class Connection extends \ManiaLive\Database\Connection
 	function query($query)
 	{
 		Connection::startMeasuring($this);
+		if(func_num_args() > 1)
+		{
+			$query = call_user_func_array('sprintf', func_get_args());
+		}
 		$result = mysql_query($query, $this->connection);
 		Connection::endMeasuring($this);
-		
+
 		if(!$result)
 		{
 			throw new QueryException(mysql_error($this->connection), mysql_errno($this->connection));
 		}
 		return new RecordSet($result);
 	}
-	
+
 	function execute($query)
 	{
 		Connection::startMeasuring($this);
+		if(func_num_args() > 1)
+		{
+			$query = call_user_func_array('sprintf', func_get_args());
+		}
 		$result = mysql_unbuffered_query($query);
 		Connection::endMeasuring($this);
-		
+
 		if (!$result)
 		{
 			throw new QueryException(mysql_error($this->connection), mysql_errno($this->connection));
 		}
 	}
-	
+
 	function affectedRows()
 	{
 		return mysql_affected_rows($this->connection);
 	}
-	
+
 	function insertID()
 	{
 		return mysql_insert_id($this->connection);
 	}
-	
+
 	function isConnected()
 	{
-		return (!$this->connection); 
+		return (!$this->connection);
 	}
-	
+
 	function disconnect()
 	{
 		if(!mysql_close($this->connection))
@@ -137,12 +145,12 @@ class Connection extends \ManiaLive\Database\Connection
 			throw new DisconnectionException;
 		}
 	}
-	
+
 	function getDatabase()
 	{
 		return $this->database;
 	}
-	
+
 	function tableExists($table_name)
 	{
 		$table = $this->query("SHOW TABLES LIKE '".$table_name."'");
--- libraries/ManiaLive/Database/SQLite/Connection.php
+++ libraries/ManiaLive/Database/SQLite/Connection.php
@@ -17,79 +17,87 @@ class Connection extends \ManiaLive\Database\Connection
 {
 	protected $connection;
 	protected $filename;
-	
+
 	function __construct($host, $username, $password, $database, $port)
 	{
 		// create the data subfolder in root directory ...
 		$datapath = APP_ROOT . 'data';
 		if (!file_exists($datapath))
-			mkdir($datapath);
-		
+		mkdir($datapath);
+
 		// move the database file in data subfolder ...
 		$this->filename = $datapath.'/'.$host . '.db';
-		
+
 		// create new connection ...
 		$this->connection = sqlite_open($this->filename);
-		
+
 		// check
 		if (!$this->connection)
 		{
 			throw new ConnectionException;
 		}
-		
+
 		// don't need to set utf8 ...
 	}
-	
+
 	function setCharset($charset)
 	{
 		throw new NotSupportedException;
 	}
-	
+
 	function isConnected()
 	{
 		return ($this->connection != null && $this->connection != false);
 	}
-	
+
 	function getDatabase()
 	{
 		return $this->filename;
 	}
-	
+
 	function affectedRows()
 	{
 		return sqlite_changes($this->connection);
 	}
-	
+
 	function insertID()
 	{
 		return sqlite_last_insert_rowid($this->connection);
 	}
-	
+
 	function query($query)
 	{
 		if (!$this->isConnected())
-			throw new NotConnectedException;
-		
+		throw new NotConnectedException;
+
 		Connection::startMeasuring($this);
+		if(func_num_args() > 1)
+		{
+			$query = call_user_func_array('sprintf', func_get_args());
+		}
 		$result = sqlite_query($this->connection, $query);
 		Connection::endMeasuring($this);
-		
+
 		if (!$result)
 		{
 			$errno = sqlite_last_error($this->connection);
 			$errstr = sqlite_error_string($errno);
 			throw new QueryException($errstr, $errno);
 		}
-		
+
 		return new RecordSet($result);
 	}
-	
+
 	function execute($query)
 	{
 		Connection::startMeasuring($this);
+		if(func_num_args() > 1)
+		{
+			$query = call_user_func_array('sprintf', func_get_args());
+		}
 		$result = sqlite_exec($this->connection, $query);
 		Connection::endMeasuring($this);
-		
+
 		if ($result === false)
 		{
 			$errno = sqlite_last_error($this->connection);
@@ -97,7 +105,7 @@ class Connection extends \ManiaLive\Database\Connection
 			throw new QueryException($errstr, $errno);
 		}
 	}
-	
+
 	function disconnect()
 	{
 		if (!sqlite_close($this->connection))
@@ -105,17 +113,17 @@ class Connection extends \ManiaLive\Database\Connection
 			throw new DisconnectionException;
 		}
 	}
-	
+
 	function quote($string)
 	{
 		return sqlite_escape_string($string);
 	}
-	
+
 	function select($database)
 	{
 		throw new NotSupportedException;
 	}
-	
+
 	function tableExists($table_name)
 	{
 		$table = $this->query("SELECT name FROM sqlite_master WHERE name='" . $table_name . "' AND type='table'");
