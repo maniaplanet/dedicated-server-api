4ce744505b479ba7136a2a262d71fc8a6bd00477
floschnell <floschnell@googlemail.com>
2010-12-29 16:35:56 +0000

    auto checking for updates regularly.
    git-svn-id: http://manialive.googlecode.com/svn/trunk@80 282cbf28-0b21-c27b-f752-b18d30e2b6b0

---
+++ libraries/ManiaLive/Features/Updater.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ManiaLive\Features;
+
+use ManiaLive\Utilities\Console;
+use ManiaLive\Event\Dispatcher;
+
+/**
+ * Checks for updates periodically.
+ * @author Florian Schnell
+ * @copyright 2010 NADEO
+ */
+class Updater extends \ManiaLive\Utilities\Singleton
+implements \ManiaLive\Features\Tick\Listener
+{
+	protected $lastDisplayed;
+	
+	protected function __construct()
+	{
+		$this->lastDisplayed = 0;
+		Dispatcher::register(\ManiaLive\Features\Tick\Event::getClass(), $this);
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see libraries/ManiaLive/Features/Tick/ManiaLive\Features\Tick.Listener::onTick()
+	 */
+	function onTick()
+	{
+		if ($this->lastDisplayed < time())
+		{
+			$this->checkUpdate();
+			$this->lastDisplayed = time() + 3600;
+		}
+	}
+	
+	/**
+	 * Routine that compares version number to
+	 * the latest version online.
+	 */
+	function checkUpdate()
+	{
+		$version = intval(file_get_contents('http://manialink.manialive.com/public/version'));
+		
+		if ($version > \ManiaLiveApplication\Version)
+		{
+			Console::println('###############################################################################');
+			Console::println('#                                                                             #');
+			Console::println('#                  A new version of ManiaLive is available!                   #');
+			Console::println('#                    An update is strongly recommended!                       #');
+			Console::println('#                     Download it at www.manialive.com                        #');
+			Console::println('#                                                                             #');
+			Console::println('###############################################################################');
+		}
+	}
+}
+
+?>
