e436ea6e883213a8937ac8f4854fcfe9b028cdce
floschnell <floschnell@googlemail.com>
2011-01-06 16:52:07 +0000

    added documentation and applied coding guidelines.
    git-svn-id: http://manialive.googlecode.com/svn/trunk@121 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- update/update.php
+++ update/update.php
@@ -9,7 +9,11 @@ echo NL;
 
 echo '> Checking local ManiaLive version ...' . NL;
 
-if (file_exists('../noupdate')) die('ERROR: This version is locked for updates!' . NL);
+// the noupdate file will prevent the updater from working its magic.
+if (file_exists('../noupdate')) 
+{
+	die('ERROR: This version is locked for updates!' . NL);
+}
 
 $includes = array(
 	'../libraries/ManiaLive/Utilities/Singleton.php',
@@ -17,44 +21,63 @@ $includes = array(
 	'../libraries/ManiaLiveApplication/Application.php'
 );
 
+// start loading dependencies
 $success = true;
 foreach ($includes as $inc)
-	if (file_exists($inc)) include_once($inc); else $success = false;
+{
+	if (file_exists($inc))
+	{
+		include_once($inc);
+	}
+	else
+	{
+		$success = false;
+	}
+}
 
+// if everything's there, then get the version number
 if ($success)
-	$version_local = \ManiaLiveApplication\Version;
-else
-	$version_local = 0;
+{
+	$versionLocal = \ManiaLiveApplication\Version;
+}
+else // otherwise something broke and we need to do an update!
+{
+	$versionLocal = 0;
+}
 
-echo '> ManiaLive is at version ' . $version_local . NL;
+echo '> ManiaLive is at version ' . $versionLocal . NL;
 
 echo '> Checking remote ManiaLive version ...' . NL;
 
 try
 {
-	$version_remote = file_get_contents('http://manialink.manialive.com/public/version');
-	$version_remote = intval($version_remote);
+	// connect to manialive server to get the latest release's version.
+	$versionRemote = file_get_contents('http://manialink.manialive.com/public/version');
+	$versionRemote = intval($versionRemote);
 }
 catch (\Exception $ex)
 {
 	die('ERROR: Could not get the remote version!'. NL);
 }
 
-echo '> Remote ManiaLive is at version ' . $version_remote . NL;
+echo '> Remote ManiaLive is at version ' . $versionRemote . NL;
 
-if ($version_local >= $version_remote)
+// no need to update when there's already the latest version installed
+if ($versionLocal >= $versionRemote)
+{
 	die ('> Local version is already uptodate, taking no action!' . NL);
+}
 
 echo '> Searching ManiaLive release package ...' . NL;
 
-$package = 'ManiaLive_1.0_r' . $version_remote . '.zip';
+$package = 'ManiaLive_1.0_r' . $versionRemote . '.zip';
 $url = 'http://manialive.googlecode.com/files/' . $package . '';
 
 if (file_exists($url)) die('ERROR: File could not be found!');
 
 echo "> downloading '" . $package . "' ..." .NL;
 
-// download and save the package 
+// download and save the package
 $data = file_get_contents($url);
 file_put_contents('./temp/' . $package, $data);
 
@@ -70,20 +93,24 @@ $zip = new ZipArchive;
 $res = $zip->open('./temp/' . $package);
 
 if ($res !== true)
-	die('ERROR: Could not extract zip archive!');
+die('ERROR: Could not extract zip archive!');
 
 $zip->extractTo('./temp/');
 $zip->close();
 
+echo NL;
+
 echo 'Removing old directories ...' . NL;
-deleteDir('../libraries/ManiaLive');
-deleteDir('../libraries/ManiaLiveApplication');
-deleteDir('../libraries/ManiaHome');
+rrmdir('../libraries/ManiaLive');
+rrmdir('../libraries/ManiaLiveApplication');
+rrmdir('../libraries/ManiaHome');
 unlink('../bootstrapper.php');
 unlink('../utils.inc.php');
 unlink('../LICENSE');
 unlink('../README');
 unlink('../CONVENTIONS');
+
+echo NL;
 
 echo 'Copying new files ...' . NL;
 rcopy('./temp/ManiaLive/libraries/ManiaLive', '../libraries/ManiaLive');
@@ -95,57 +122,73 @@ copy('./temp/ManiaLive/LICENSE', '../LICENSE');
 copy('./temp/ManiaLive/README', '../README');
 copy('./temp/ManiaLive/CONVENTIONS', '../CONVENTIONS');
 
+echo NL;
+
 echo '> Cleaning up ...' . NL;
 
 deleteDir('./temp/ManiaLive');
 
 echo '>> Done!' . NL;
 
-function deleteDir($dir) 
-{ 
+/**
+ * Recursively remove a directory.
+ * @param $dir Directory to remove.
+ */
+function rrmdir($dir)
+{
 	if (!is_dir($dir)) return;
 
-   if (substr($dir, strlen($dir)-1, 1) != '/') 
-       $dir .= '/'; 
+	if (substr($dir, strlen($dir)-1, 1) != '/')
+	$dir .= '/';
 
-   echo 'deleting: ' . $dir . NL; 
+	echo 'deleting: ' . $dir . NL;
 
-   if ($handle = opendir($dir)) 
-   { 
-       while ($obj = readdir($handle)) 
-       { 
-           if ($obj != '.' && $obj != '..') 
-           { 
-               if (is_dir($dir.$obj)) 
-               { 
-                   deleteDir($dir.$obj);
-               } 
-               elseif (is_file($dir.$obj)) 
-               { 
-                   unlink($dir.$obj);
-               } 
-           } 
-       } 
-       closedir($handle); 
-       return rmdir($dir);
-   } 
-   return false; 
-}  
+	if ($handle = opendir($dir))
+	{
+		while ($obj = readdir($handle))
+		{
+			if ($obj != '.' && $obj != '..')
+			{
+				if (is_dir($dir.$obj))
+				{
+					rrmdir($dir.$obj);
+				}
+				elseif (is_file($dir.$obj))
+				{
+					unlink($dir.$obj);
+				}
+			}
+		}
+		closedir($handle);
+		return rmdir($dir);
+	}
+	return false;
+}
 
-function rcopy($src, $dst) { 
-	$dir = opendir($src); 
+/**
+ * Recursively copy a directory.
+ * @param string $src Source directory.
+ * @param string $dst Destination directory.
+ */
+function rcopy($src, $dst)
+{
+	$dir = opendir($src);
 	@mkdir($dst);
-	while(false !== ( $file = readdir($dir)) ) { 
-		if (( $file != '.' ) && ( $file != '..' )) { 
-			if ( is_dir($src . '/' . $file) ) { 
-				rcopy($src . '/' . $file, $dst . '/' . $file); 
-			} 
-			else { 
+	while(false !== ($file = readdir($dir)))
+	{
+		if (($file != '.') && ($file != '..'))
+		{
+			if (is_dir($src . '/' . $file))
+			{
+				rcopy($src . '/' . $file, $dst . '/' . $file);
+			}
+			else
+			{
 				echo 'copying: ' . $src . NL;
-				copy($src . '/' . $file, $dst . '/' . $file); 
-			} 
-		} 
-	} 
-	closedir($dir); 
+				copy($src . '/' . $file, $dst . '/' . $file);
+			}
+		}
+	}
+	closedir($dir);
 }
 ?>
