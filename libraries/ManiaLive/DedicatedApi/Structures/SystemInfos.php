794fcd72217cba7db62b424ecdae5fff6a62046e
Philippe Melot <melot.philippe@gmail.com>
2011-01-07 15:39:17 +0000

    set keywords properties
    git-svn-id: http://manialive.googlecode.com/svn/trunk@131 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLive/DedicatedApi/Structures/SystemInfos.php
+++ libraries/ManiaLive/DedicatedApi/Structures/SystemInfos.php
@@ -5,9 +5,9 @@
  * 
  * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
- * @version     $Revision: 1709 $:
- * @author      $Author: svn $:
- * @date        $Date: 2011-01-07 14:06:13 +0100 (ven., 07 janv. 2011) $:
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
  */
 namespace ManiaLive\DedicatedApi\Structures;
 
