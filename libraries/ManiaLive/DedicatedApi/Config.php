b04b1eb7c59677ea13e9c4c55005f4f80b2d83b5
Philippe Melot <melot.philippe@gmail.com>
2011-01-07 15:14:47 +0000

    Edit all files header
    git-svn-id: http://manialive.googlecode.com/svn/trunk@130 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLive/DedicatedApi/Config.php
+++ libraries/ManiaLive/DedicatedApi/Config.php
@@ -1,6 +1,12 @@
 <?php
 /**
- * @copyright NADEO (c) 2010
+ * ManiaLive - TrackMania dedicated server manager in PHP
+ * 
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision: 1709 $:
+ * @author      $Author: svn $:
+ * @date        $Date: 2011-01-07 14:06:13 +0100 (ven., 07 janv. 2011) $:
  */
 
 namespace ManiaLive\DedicatedApi;
