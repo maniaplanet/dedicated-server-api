a8de5bdd3a36a7a3094573a5eb962b6fd24a5106
Philippe Melot <melot.philippe@gmail.com>
2011-06-13 15:15:08 +0000

    correct ManiaHomeClient to make it compatible with the latest version of ManiaHome ! Remove support of type & change methods signature
    git-svn-id: http://manialive.googlecode.com/svn/trunk@227 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaHome/ManiaHomeClient.php
+++ libraries/ManiaHome/ManiaHomeClient.php
@@ -7,53 +7,46 @@ namespace ManiaHome;
 
 use ManiaLive\Config\Loader;
 
-define('APP_MANIAHOME_SERVICES_URL'	, 'http://maniahome.trackmania.com/services/?');
-
-use ManiaHome\Rest\RestClient;
-
 class ManiaHomeClient
 {
-	const NONE 		= 0;
-	const TRACK 	= 1;
-	const REPLAY 	= 2;
-	
-	protected $provider;
+    protected $provider;
     protected $restClient;
-	
-    static function sendNotificationToPlayer($message, $login, $link, $type = self::NONE, $iconStyle = null, $iconSubStyle = null)
-	{
-		if (Loader::$config->maniahome->enabled)
-		{
-			$maniahome_client = new static();
+
+    static function sendNotificationToPlayer($message, $login, $link, $iconStyle = null, $iconSubStyle = null)
+    {
+	   if(Loader::$config->maniahome->enabled)
+	   {
+		  $maniahome_client = new static();
 		  $maniahome_client->send($message, $login, $link, $type, $iconStyle, $iconSubStyle);
-		}
-	}
-	
-	protected function __construct()
-	{
+	   }
+    }
+
+    protected function __construct()
+    {
 	   $this->restClient = new \ManiaLib\Rest\Client(Loader::$config->maniahome->user, Loader::$config->maniahome->password);
-		$this->provider = Loader::$config->maniahome->manialink;
-	}
-	
-    protected function send($message, $login, $link, $type = self::NONE, $iconStyle = null, $iconSubStyle = null)
-	{
-		
-		$body = array(
-			'message' => $message,
+	   $this->provider = Loader::$config->maniahome->manialink;
+    }
+
+    protected function send($message, $login, $link, $iconStyle = null, $iconSubStyle = null)
+    {
+	   $body = array(
+		  'message' => $message,
 		  'senderName' => $this->provider,
-		  'receiverName' => $player,
-            'link' => $link,
-		  'type' => $type,
+		  'receiverName' => $login,
+		  'link' => $link,
 		  'iconStyle' => $iconStyle,
 		  'iconStyle' => $iconSubStyle,
-		);
+	   );
 	   try
-		{
-		  $this->restClient->execute('POST', '/maniahome/notification/', $body);
-		}
+	   {
+		  $this->restClient->execute('POST', '/maniahome/notification/%s/', array($login, $body));
+	   }
 	   catch(\Exception $e)
-		{
-		}
-	}
+	   {
+
+	   }
+    }
+
 }
+
 ?>
