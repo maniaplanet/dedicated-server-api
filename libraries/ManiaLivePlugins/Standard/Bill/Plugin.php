64125d6dcca33626d81873f9c7042cfde4ae3162
Philippe Melot <melot.philippe@gmail.com>
2011-06-15 10:37:51 +0000

    git-svn-id: http://manialive.googlecode.com/svn/trunk@235 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLivePlugins/Standard/Bill/Plugin.php
+++ libraries/ManiaLivePlugins/Standard/Bill/Plugin.php
@@ -16,42 +16,34 @@ use ManiaLib\Filters\Validation;
 
 class Plugin extends \ManiaLive\PluginHandler\Plugin
 {
-	public static $dbUser = 'root';
-	public static $dbPass = '';
-	public static $dbHost = 'localhost';
-	public static $dbName = 'manialive';
 
-	protected $dbInstance;
-
-	function onInit()
+    function onInit()
 	{
 		$this->setVersion(1.0);
 	}
 
 	function onLoad()
 	{
-		$this->dbInstance = Connection::getConnection(self::$dbHost, self::$dbUser, self::$dbPass, self::$dbName);
-		
 		$command = $this->registerChatCommand('pay', 'toServer', 1, true);
 		$command->help = 'Give the amount to the server'."\n".
 		'sample of usage: /pay 42 will give 42 coppers to the server';
-		
+
 		$command = $this->registerChatCommand('pay', 'toPlayer', 2, true);
 		$command->help = 'Give the amount to player'."\n".
 		'sample of usage: /pay playerLogin 42 will give 42 coppers to the player playerLogin';
-		
+
 		$command = $this->registerChatCommand('give', 'fromServer', 2, true, AdminGroup::get());
 		$command->help = 'Give the amount to the player'."\n".
 		'sample of usage: /give playerLogin 42 will give 42 coppers to the player playerLogin with the coppers of the server account';
-		
+
 		$command = $this->registerChatCommand('checkBill', 'checkBill', 1, true);
 		$command->help = 'Give the current state of the bill'."\n".
 		'sample of usage: /give 42 where 42 is the id of the bill';
-		
+
 		$this->setPublicMethod('payFromServer');
 		$this->setPublicMethod('payToServer');
 	}
-	
+
 	function payFromServer($pluginId, $payee, $amount, $label)
 	{
 		$this->fromServer('', $payee, $amount, $label);
@@ -60,12 +52,12 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 	function fromServer($login, $payee, $amount, $label = '')
 	{
 		Validation::int($amount,0);
-		$label = $label ? $label : $this->storage->serverLogin.'To'.$payee; 
+		$label = $label ? $label : $this->storage->serverLogin.'To'.$payee;
 		$billId = $this->connection->sendBill($this->storage->serverLogin, $amount, $label, $payee);
 		$player = ($login ? $this->storage->getPlayerObject($login) : AdminGroup::get());
 		$this->connection->chatSendServerMessage('The bill has been created with the id '.$billId, $player, true);
 	}
-	
+
 	function payToServer($pluginId, $playerLogin, $amount, $label)
 	{
 		$this->connection->chatSendServerMessage('The plugin '.$pluginId.' has created a bill for you to the server of '.$amount.' coppers');
@@ -79,7 +71,7 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 		{
 			return;
 		}
-		
+
 		try
 		{
 			Validation::int($amount,0);
@@ -89,9 +81,9 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 				$this->connection->chatSendServerMessage('$F00The amount is incorrect', $player, true);
 				return;
 		}
-		
+
 		$label = $label ? $label : $player->login.'ToServer'.$this->storage->serverLogin;
-		
+
 		$billId = $this->connection->sendBill($player->login, $amount, $label);
 		$this->connection->chatSendServerMessage('The bill has been created with the id '.$billId, $player, true);
 	}
@@ -103,14 +95,14 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 			$this->connection->chatSendServerMessage('$F00You can\'t give coppers to yourself', $player, true);
 			return;
 		}
-		
+
 		$payer = $this->storage->getPlayerObject($payerLogin);
-		
+
 		if(!$player)
 		{
 			return;
 		}
-		
+
 		try
 		{
 			Validation::int($amount,0);
@@ -120,15 +112,15 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 				$this->connection->chatSendServerMessage('$F00The amount is incorrect', $player, true);
 				return;
 		}
-		
+
 		$billId = $this->connection->sendBill($payer->login, $amount, $payer->login.'To'.$payeeLogin, $payeeLogin);
 		$this->connection->chatSendServerMessage('The bill has been created with the id '.$billId, $player, true);
 	}
-	
+
 	function checkBill($login, $billId)
 	{
 		$bill = $this->connection->getBillState($billId);
-		
+
 		$this->connection->chatSendServerMessage('The bill n°'.$billId.' is currently $<$o'.$bill->stateName.'$>', $this->storage->getPlayerObject($login),true);
 	}
 }
