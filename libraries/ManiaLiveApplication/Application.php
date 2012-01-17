df788c875b3411d577d325da78070677ad882150
Martin Gwendal <martin.gwendal@gmail.com>
2012-01-17 17:01:02 +0000

    Preparing new release, starting the new version numbering
    git-svn-id: http://manialive.googlecode.com/svn/trunk@333 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = 322;
+const Version = '2.0';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
