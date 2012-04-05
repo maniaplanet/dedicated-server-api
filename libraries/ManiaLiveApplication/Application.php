48dc497a9ff9481fb2e28db06ff245ca5153e223
Martin Gwendal <martin.gwendal@gmail.com>
2012-04-05 08:29:24 +0000

    Preparing new version Fixing some line endings
    git-svn-id: http://manialive.googlecode.com/svn/trunk@355 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = '2.1.2';
+const Version = '2.1.3';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
