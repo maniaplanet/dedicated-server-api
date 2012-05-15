7e52fa24f05951bee8e920d6f73659fbfe06b159
Martin Gwendal <martin.gwendal@gmail.com>
2012-05-15 10:48:06 +0000

    Changing SQLite support to v3 only (forgot bootstrapper.php -_-)
    git-svn-id: http://manialive.googlecode.com/svn/trunk@370 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- bootstrapper.php
+++ bootstrapper.php
@@ -9,21 +9,19 @@
  * @date        $Date$:
  */
 
-$php_ok = (function_exists('version_compare') && version_compare(phpversion(), '5.3.1', '>='));
-$json_ok = (extension_loaded('json') && function_exists('json_encode') && function_exists('json_decode'));
-$spl_ok = extension_loaded('spl');
-$curl_ok = function_exists('curl_version');
-if($curl_ok)
+$phpOk = (function_exists('version_compare') && version_compare(phpversion(), '5.3.1', '>='));
+$jsonOk = (extension_loaded('json') && function_exists('json_encode') && function_exists('json_decode'));
+$splOk = extension_loaded('spl');
+$curlOk = function_exists('curl_version');
+if($curlOk)
 {
-	$curl_version = curl_version();
-	$curl_ssl_ok = (function_exists('curl_exec') && in_array('https', $curl_version['protocols'], true));
+	$curlVersion = curl_version();
+	$curlSslOk = (function_exists('curl_exec') && in_array('https', $curlVersion['protocols'], true));
 }
-$sqlite2_ok = extension_loaded('sqlite');
-$sqlite3_ok = extension_loaded('sqlite3');
-$sqlite_ok = ($sqlite2_ok || $sqlite3_ok);
+$sqlite3Ok = extension_loaded('sqlite3');
 
 echo '
- _|      _|                      _|            _|        _|                        
+ _|      _|                      _|            _|        _|
  _|_|  _|_|    _|_|_|  _|_|_|          _|_|_|  _|            _|    _|    _|_|
  _|  _|  _|  _|    _|  _|    _|  _|  _|    _|  _|        _|  _|    _|  _|_|_|_|
  _|      _|  _|    _|  _|    _|  _|  _|    _|  _|        _|  _|  _|    _|
@@ -32,15 +30,15 @@ echo '
 echo '-----------------------------------------------------'.PHP_EOL;
 echo 'PHP Environment Compatibility Test'.PHP_EOL;
 echo '-----------------------------------------------------'.PHP_EOL;
-echo 'PHP 5.3.1 or newer    -> required  -> ' . ($php_ok ? ('[ Yes ] '.phpversion()) : '[ No  ]').PHP_EOL;
-echo 'Standard PHP Library  -> required  -> ' . ($spl_ok ? '[ Yes ]' : '[ No  ]').PHP_EOL;
-echo 'JSON                  -> required  -> ' . ($json_ok ? '[ Yes ]' : '[ No  ]').PHP_EOL;
-echo 'cURL with SSL         -> required  -> ' . ($curl_ok ? ($curl_ssl_ok ? '[ Yes ] '.$curl_version['version'].' (with '.$curl_version['ssl_version'].')' : '[ No  ] '.$curl_version['version'].' (without SSL)') : '[ No  ]').PHP_EOL;
-echo 'SQLite                -> optional  -> ' . ($sqlite_ok ? '[ Yes ]' : '[ No  ]').PHP_EOL;
+echo 'PHP 5.3.1 or newer    -> required  -> ' . ($phpOk ? ('[ Yes ] '.phpversion()) : '[ No  ]').PHP_EOL;
+echo 'Standard PHP Library  -> required  -> ' . ($splOk ? '[ Yes ]' : '[ No  ]').PHP_EOL;
+echo 'JSON                  -> required  -> ' . ($jsonOk ? '[ Yes ]' : '[ No  ]').PHP_EOL;
+echo 'cURL with SSL         -> required  -> ' . ($curlOk ? ($curlSslOk ? '[ Yes ] '.$curlVersion['version'].' (with '.$curlVersion['ssl_version'].')' : '[ No  ] '.$curlVersion['version'].' (without SSL)') : '[ No  ]').PHP_EOL;
+echo 'SQLite3               -> optional  -> ' . ($sqlite3Ok ? '[ Yes ]' : '[ No  ]').PHP_EOL;
 echo '-----------------------------------------------------'.PHP_EOL;
 
 
-if(!$php_ok || !$curl_ok || !$spl_ok || !$json_ok)
+if(!$phpOk || !$curlOk || !$splOk || !$jsonOk)
 {
     echo 'Your system is not compatible, check your php configuration.'.PHP_EOL;
     exit;
@@ -55,7 +53,7 @@ if(!ini_get('date.timezone'))
 	date_default_timezone_set($timezone);
 }
 
-if(!$sqlite_ok)
+if(!$sqlite3Ok)
 {
     echo 'SQLite is disabled, threading will not work. ManiaLive may encounter some perfomance trouble.'.PHP_EOL;
 }
