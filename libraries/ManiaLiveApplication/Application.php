97bb1e730baab6fc3adddb190f8bfba56cc6d20e
Philippe Melot <melot.philippe@gmail.com>
2012-06-14 13:03:18 +0000

    prepare new release
    git-svn-id: http://manialive.googlecode.com/svn/trunk@397 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = '2.1.9';
+const Version = '2.1.10';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
