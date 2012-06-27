fdc1c25da3d23117a9dfed7deb31fe86976ff4f0
Martin Gwendal <martin.gwendal@gmail.com>
2012-06-27 10:06:51 +0000

    Preparing version 2.1.11
    git-svn-id: http://manialive.googlecode.com/svn/trunk@404 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = '2.1.10';
+const Version = '2.1.11';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
