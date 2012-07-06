2c6bb49fc563c37139f09ba7eb2f108496532cc8
Martin Gwendal <martin.gwendal@gmail.com>
2012-07-06 11:25:33 +0000

    Adding TrustCircle Standard plugin Updating WS SDK external to 1.5
    git-svn-id: http://manialive.googlecode.com/svn/trunk@412 282cbf28-0b21-c27b-f752-b18d30e2b6b0

---
+++ libraries/ManiaLivePlugins/Standard/TrustCircle/Config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright   Copyright (c) 2009-2012 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision: $:
+ * @author      $Author: $:
+ * @date        $Date: $:
+ */
+
+namespace ManiaLivePlugins\Standard\TrustCircle;
+
+class Config extends \ManiaLib\Utils\Singleton
+{
+	public $readFrom = array();
+	public $rules = array();
+}
+
+?>
