4e3ce4089915013c6c2ee9f8150dbe5a0bf07901
Martin Gwendal <martin.gwendal@gmail.com>
2012-07-06 11:11:29 +0000

    Changing version to 2.1.12
    git-svn-id: http://manialive.googlecode.com/svn/trunk@410 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = '2.1.11';
+const Version = '2.1.12';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
