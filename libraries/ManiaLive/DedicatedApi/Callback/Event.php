d60f5d0e9b9725a05e950f8d352bb020a79626a9
Martin Gwendal <martin.gwendal@gmail.com>
2012-07-13 13:39:35 +0000

    Fixed onBeginMap and onEndMap while running a SM server
    git-svn-id: http://manialive.googlecode.com/svn/trunk@419 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLive/DedicatedApi/Callback/Event.php
+++ libraries/ManiaLive/DedicatedApi/Callback/Event.php
@@ -36,7 +36,7 @@ class Event extends \ManiaLive\Event\Event
 	const ON_PLAYER_INFO_CHANGED            = 0x100000;
 	const ON_MANUAL_FLOW_CONTROL_TRANSITION = 0x200000;
 	const ON_VOTE_UPDATED                   = 0x400000;
-	const ON_MODE_SCRIPT_CALLBACK          = 0x800000;
+	const ON_MODE_SCRIPT_CALLBACK           = 0x800000;
 	
 	static private $rc;
 	protected $params;
@@ -62,8 +62,20 @@ class Event extends \ManiaLive\Event\Event
 			case self::ON_SERVER_STOP: $listener->onServerStop(); break;
 			case self::ON_BEGIN_MATCH: $listener->onBeginMatch($p[0]); break;
 			case self::ON_END_MATCH: $listener->onEndMatch($p[0], $p[1]); break;
-			case self::ON_BEGIN_MAP: $listener->onBeginMap($p[0], $p[1], $p[2]); break;
-			case self::ON_END_MAP: $listener->onEndMap($p[0], $p[1], $p[2], $p[3], $p[4]); break;
+			case self::ON_BEGIN_MAP:
+				// FIXME hack for SM
+				if(count($p) == 1)
+					$listener->onBeginMap($p[0], null, null);
+				else
+					$listener->onBeginMap($p[0], $p[1], $p[2]);
+				break;
+			case self::ON_END_MAP:
+				// FIXME hack for SM
+				if(count($p) == 1)
+					$listener->onEndMap(null, $p[0], null, null, null);
+				else
+					$listener->onEndMap($p[0], $p[1], $p[2], $p[3], $p[4]);
+				break;
 			case self::ON_BEGIN_ROUND: $listener->onBeginRound(); break;
 			case self::ON_END_ROUND: $listener->onEndRound(); break;
 			case self::ON_STATUS_CHANGED: $listener->onStatusChanged($p[0], $p[1]); break;
--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = '2.1.13';
+const Version = '2.1.14';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
