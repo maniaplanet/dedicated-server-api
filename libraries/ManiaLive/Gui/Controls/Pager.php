ca710032882f688874f44bed94dbfcfc2f9ae072
Philippe Melot <melot.philippe@gmail.com>
2013-02-25 14:17:10 +0000

    correct pager
    git-svn-id: http://manialive.googlecode.com/svn/trunk@477 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLive/Gui/Controls/Pager.php
+++ libraries/ManiaLive/Gui/Controls/Pager.php
@@ -29,7 +29,7 @@ class Pager extends \ManiaLive\Gui\Control
 	private $buttonPrev;
 	private $container;
 	private $label;
-	
+
 	private $actionNext;
 	private $actionPrev;
 	
@@ -46,80 +46,80 @@ class Pager extends \ManiaLive\Gui\Control
 		$this->currentPage = 0;
 		$this->stretchContentX = false;
 		$this->needRefresh = false;
-		
+
 		$this->actionNext = $this->createAction(array($this, 'nextPage'));
 		$this->actionPrev = $this->createAction(array($this, 'previousPage'));
-		
+
 		$this->buttonNext = new Icons64x64_1(9);
 		$this->buttonNext->setSubStyle(Icons64x64_1::ArrowNext);
 		$this->addComponent($this->buttonNext);
-		
+
 		$this->buttonPrev = new Icons64x64_1(9);
 		$this->buttonPrev->setSubStyle(Icons64x64_1::ArrowPrev);
 		$this->addComponent($this->buttonPrev);
-		
-		$this->container = new Frame(2, 0, new Column(0, 0, Column::DIRECTION_DOWN));
+
+		$this->container = new Frame(0, 0, new Column());
 		$this->addComponent($this->container);
-		
+
 		$this->label = new Label();
 		$this->label->setHalign('center');
 		$this->addComponent($this->label);
 	}
-	
+
 	function addItem(Component $item)
 	{
 		$this->items[spl_object_hash($item)] = $item;
 		$this->needRefresh = true;
 	}
-	
+
 	function removeItem(Component $item)
 	{
 		unset($this->items[spl_object_hash($item)]);
 		$this->needRefresh = true;
 	}
-	
+
 	function clearItems()
 	{
 		$this->items = array();
 		$this->needRefresh = true;
 	}
-	
+
 	function orderfromTopToBottom()
 	{
 		$this->container->getLayout()->setDirection($this->direction);
 		$this->container->setPosY(0);
 		$this->needRefresh = true;
 	}
-	
+
 	function orderFromBottomToTop()
 	{
 		$this->container->getLayout()->setDirection($this->direction);
 		$this->container->setPosY(5.3 - $this->sizeY);
 		$this->needRefresh = true;
 	}
-	
+
 	function setStretchContentX($stretch)
 	{
 		$this->stretchContentX = $stretch;
 	}
-	
+
 	function getPages()
 	{
 		return count($this->pages);
 	}
-	
+
 	function nextPage($login)
 	{
 		$this->currentPage++;
 		$this->redraw();
 	}
-	
+
 	function previousPage($login)
 	{
 		$this->currentPage--;
 		$this->redraw();
 	}
-	
+
 	protected function onResize($oldX, $oldY)
 	{
 		$this->buttonPrev->setPosition(0, 5 - $this->sizeY);
@@ -128,15 +128,15 @@ class Pager extends \ManiaLive\Gui\Control
 		if($this->container->getPosY())
 			$this->container->setPosY(5.3 - $this->sizeY);
 	}
-	
+
 	function onDraw()
 	{
 		if($this->needRefresh)
 			$this->refreshPages();
-		
+
 		// refresh container components.
 		$this->container->clearComponents();
-		
+
 		if(isset($this->pages[$this->currentPage]))
 		{
 			foreach($this->pages[$this->currentPage] as $item)
@@ -146,45 +146,55 @@ class Pager extends \ManiaLive\Gui\Control
 				$this->container->addComponent($item);
 			}
 		}
-		
+
 		// page label
-		$this->label->setText(($this->currentPage + 1).' / '.count($this->pages));
+		if(count($this->pages) > 1)
+		{
+			$this->label->setText(($this->currentPage + 1).' / '.count($this->pages));
 		
-		// draw forward and back buttons
-		if($this->currentPage <= 0)
-		{
-			$this->buttonPrev->setAction(null);
-			$this->buttonPrev->setSubStyle(Icons64x64_1::ClipPause);
-		}
-		else 
-		{
-			$this->buttonPrev->setSubStyle(Icons64x64_1::ArrowPrev);
-			$this->buttonPrev->setAction($this->actionPrev);
-		}
-		
-		if($this->currentPage >= count($this->pages) - 1)
-		{
-			$this->buttonNext->setAction(null);
-			$this->buttonNext->setSubStyle(Icons64x64_1::ClipPause);
+			// draw forward and back buttons
+			if($this->currentPage <= 0)
+			{
+				$this->buttonPrev->setAction(null);
+				$this->buttonPrev->setSubStyle(Icons64x64_1::ClipPause);
+			}
+			else
+			{
+				$this->buttonPrev->setSubStyle(Icons64x64_1::ArrowPrev);
+				$this->buttonPrev->setAction($this->actionPrev);
+			}
+
+			if($this->currentPage >= count($this->pages) - 1)
+			{
+				$this->buttonNext->setAction(null);
+				$this->buttonNext->setSubStyle(Icons64x64_1::ClipPause);
+			}
+			else
+			{
+				$this->buttonNext->setAction($this->actionNext);
+				$this->buttonNext->setSubStyle(Icons64x64_1::ArrowNext);
+			}
 		}
 		else
 		{
-			$this->buttonNext->setAction($this->actionNext);
-			$this->buttonNext->setSubStyle(Icons64x64_1::ArrowNext);
+			$this->buttonPrev->setAction(null);
+			$this->buttonPrev->setSubStyle(Icons64x64_1::EmptyIcon);
+			$this->buttonNext->setAction(null);
+			$this->buttonNext->setSubStyle(Icons64x64_1::EmptyIcon);
 		}
 	}
-	
+
 	private function refreshPages()
 	{
 		$current = null;
 		if(isset($this->pages[$this->currentPage]) && isset($this->pages[$this->currentPage][0]))
-			$current = $this->pages[$this->currentPage][0];
-		
+				$current = $this->pages[$this->currentPage][0];
+
 		$this->pages = array();
 		$currentSizeY = 0;
 		$maxSizeY = $this->sizeY - 5.3;
 		$pageCount = 0;
-		
+
 		foreach($this->items as $item)
 		{
 			$currentSizeY += $item->getSizeY();
@@ -194,17 +204,17 @@ class Pager extends \ManiaLive\Gui\Control
 				++$pageCount;
 			}
 			$this->pages[$pageCount][] = $item;
-			
+
 			if($current === $item)
 				$this->currentPage = $pageCount;
 		}
 	}
-	
+
 	function destroy()
 	{
 		$this->items = null;
 		$this->pages = null;
-		
+
 		parent::destroy();
 	}
 }
