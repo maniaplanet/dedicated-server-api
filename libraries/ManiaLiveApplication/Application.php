52ef3b2c72c2dcde2dfa290131a198d7425ee4ce
magnetik <baptiste33@gmail.com>
2013-04-15 15:03:18 +0000

    Changing version to 2.7.2
    git-svn-id: http://manialive.googlecode.com/svn/trunk@513 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/ManiaLiveApplication/Application.php
+++ libraries/ManiaLiveApplication/Application.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 /**
  * ManiaLive - TrackMania dedicated server manager in PHP
- * 
+ *
  * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
  * @version     $Revision$:
@@ -13,7 +13,7 @@ namespace ManiaLiveApplication;
 
 use ManiaLive\Features\Updater;
 
-const Version = '2.7.1';
+const Version = '2.7.2';
 
 class Application extends \ManiaLive\Application\AbstractApplication
 {
@@ -21,7 +21,7 @@ class Application extends \ManiaLive\Application\AbstractApplication
 	{
 		\ManiaLive\Utilities\Console::println('Running ManiaLive '.Version.'...');
 		parent::init();
-		
+
 		if(!\ManiaLive\Config\Config::getInstance()->lanMode)
 			Updater::getInstance();
 	}
