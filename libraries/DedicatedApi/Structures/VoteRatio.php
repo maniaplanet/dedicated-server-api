1f44ad2fdfaf08e483f34339d843d9565c1b2c6f
magnetik <baptiste33@gmail.com>
2013-04-26 08:41:45 +0000

    VoteRatio constructor to shorten attribution
    git-svn-id: http://manialive.googlecode.com/svn/trunk@524 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/DedicatedApi/Structures/VoteRatio.php
+++ libraries/DedicatedApi/Structures/VoteRatio.php
@@ -2,7 +2,7 @@
 /**
  * Represents a Dedicated TrackMania Server Vote
  * ManiaLive - TrackMania dedicated server manager in PHP
- * 
+ *
  * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
  * @version     $Revision$:
@@ -16,5 +16,11 @@ class VoteRatio extends AbstractStructure
 	public $command;
 	public $param;
 	public $ratio;
+
+	public function __construct($command = null, $ratio = null)
+	{
+		$this->command = $command;
+		$this->ratio = $ratio;
+	}
 }
 ?>
