bb7489177a4a1fbf28a8f014ee63527aa5cfa4cb
magnetik <baptiste33@gmail.com>
2013-07-08 16:11:26 +0000

    Adding AutoTagManiaLive plugin
    git-svn-id: http://manialive.googlecode.com/svn/trunk@541 282cbf28-0b21-c27b-f752-b18d30e2b6b0

---
+++ libraries/ManiaLivePlugins/Standard/AutoTagMatchSettings_1/Plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+namespace ManiaLivePlugins\Standard\AutoTagManiaLive;
+
+class Plugin extends \ManiaLive\PluginHandler\Plugin
+{
+	public function onLoad()
+	{
+		$this->connection->setServerTag('nl.controller', 'ManiaLive', true);
+		$this->connection->setServerTag('nl.controller.version', \ManiaLiveApplication\Version, true);
+		$this->connection->executeMulticall();
+	}
+}
+?>
