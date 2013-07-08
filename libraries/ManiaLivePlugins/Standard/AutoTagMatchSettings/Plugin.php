3dc9417d22e3a082f1be3a4ec7f9f6fd62c9d4aa
magnetik <baptiste33@gmail.com>
2013-07-08 14:36:28 +0000

    Adding AutoTagMatchSettings plugin
    git-svn-id: http://manialive.googlecode.com/svn/trunk@538 282cbf28-0b21-c27b-f752-b18d30e2b6b0

---
+++ libraries/ManiaLivePlugins/Standard/AutoTagMatchSettings/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+namespace ManiaLivePlugins\Standard\AutoTagMatchSettings;
+
+class Plugin extends \ManiaLive\PluginHandler\Plugin
+{
+	public function onLoad()
+	{
+		$settings = $this->connection->getModeScriptSettings();
+		foreach ($settings as $setting => $value)
+		{
+			$this->connection->setServerTag($setting, $value, true);
+		}
+		$this->connection->executeMulticall();
+	}
+}
+?>
