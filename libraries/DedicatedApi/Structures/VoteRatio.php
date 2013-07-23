cfaeae2f444f1295944bf5dd486dbbf7b5f8999f
magnetik <baptiste33@gmail.com>
2013-07-23 06:56:34 +0000

    Adding const for each ratio command
    git-svn-id: http://manialive.googlecode.com/svn/trunk@549 282cbf28-0b21-c27b-f752-b18d30e2b6b0

--- libraries/DedicatedApi/Structures/VoteRatio.php
+++ libraries/DedicatedApi/Structures/VoteRatio.php
@@ -13,6 +13,15 @@ namespace DedicatedApi\Structures;
 
 class VoteRatio extends AbstractStructure
 {
+	const COMMAND_SCRIPT_SETTINGS = 'SetModeScriptSettingsAndCommands';
+	const COMMAND_NEXT_MAP = 'NextMap';
+	const COMMAND_JUMP_MAP = 'JumpToMapIndex';
+	const COMMAND_SET_NEXT_MAP = 'SetNextMapIndex';
+	const COMMAND_RESTART_MAP = 'RestartMap';
+	const COMMAND_TEAM_BALANCE = 'AutoTeamBalance';
+	const COMMAND_KICK = 'Kick';
+	const COMMAND_BAN = 'Ban';
+	
 	public $command;
 	public $param;
 	public $ratio;
