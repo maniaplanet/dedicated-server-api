a79a38990920e55974c5f722867c247b1760c550
Baptiste Lafontaine <baptiste33@gmail.com>
2014-01-16 11:48:16 +0100

    Big renaming to have new \Maniaplanet\DedicatedServer namespace

---
+++ libraries/Maniaplanet/DedicatedServer/Structures/LobbyInfo.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright   Copyright (c) 2009-2013 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+namespace Maniaplanet\DedicatedServer\Structures;
+
+class LobbyInfo extends AbstractStructure
+{
+	public $isLobby;
+	public $lobbyPlayers;
+	public $lobbyMaxPlayers;
+	public $lobbyPlayersLevel;
+}
+
+?>
