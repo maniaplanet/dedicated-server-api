f7bbe7f205745fcb4116d0e386ff17dd471c145d
Baptiste Lafontaine <baptiste@nadeo.com>
2017-07-31 11:56:41 +0200

    Better handling of errors during handshake

--- libraries/Maniaplanet/DedicatedServer/Xmlrpc/GbxRemote.php
+++ libraries/Maniaplanet/DedicatedServer/Xmlrpc/GbxRemote.php
@@ -85,6 +85,9 @@ class GbxRemote
 		// handshake
 		$header = $this->read(15);
 		if($header === false)
+			if (!is_resource($this->socket)) {
+				$this->onIoFailure('socket closed during handshake');
+			}
 			$this->onIoFailure(sprintf('during handshake (%s)', socket_strerror(socket_last_error($this->socket))));
 
 		extract(unpack('Vsize/a*protocol', $header));
